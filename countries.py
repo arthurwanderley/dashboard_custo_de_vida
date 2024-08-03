2491fd4a01937a234e344a28b368b71172954755
manucarvalho <manuela_carvalho_6@hotmail.com>
2024-08-03 15:07:45 -0300

    Países

--- countries.py
+++ countries.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd
+from utils_countries import show_grafico1
 
 def countries_page():
     st.title("Países")
+    st.markdown("Análise de dados por países")
+
+    df = pd.read_csv('df_cl_ccc_resumido.csv')
+
+    continents = df['Continent'].unique()
+
+    with st.container():
+        continent = st.radio("Selecione um continente", continents, horizontal=True)
+        #paises = sorted(df.query('Continent == @continent')['country'].unique())
+        #pais = st.multiselect("Selecione um país", paises, placeholder="Selecione")
+        #all = st.checkbox("Mostrar todos países")
+
+        show_grafico1(continent)
+
+
---
+++ utils_countries.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+def show_grafico1(continent):
+    df = pd.read_csv('df_cl_ccc_resumido.csv')
+    df_filtered = df.query('Continent == @continent').groupby('country')[['Alimentacao_Basica', 'Transp_Privado', 'Moradia_CentroCidade', 'Final_de_Semana']].mean().reset_index()
+    df_filtered.rename(columns = {'Alimentacao_Basica':'Alimentacao basica', 'Transp_Privado':'Transp privado', 
+                                 'Moradia_CentroCidade':'Moradia - centro da cidade', 'Final_de_Semana':'Final de semana'}, inplace = True)
+
+    fig = px.bar(df_filtered, x='country', y=['Alimentacao basica', 'Transp privado', 'Moradia - centro da cidade', 'Final de semana'],
+            title = 'Distribuição dos Custos de Vida',
+            labels = {'country':'Países', 'value':'Valores (USD)', 'variable':'Tipo de despesa'}
+        )
+    
+    with st.container():
+        st.plotly_chart(fig)
