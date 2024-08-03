bfd5c0fca07c16915d254041917b85a6a9f6d7a6
manucarvalho <manuela_carvalho_6@hotmail.com>
2024-08-03 17:58:59 -0300

    Ajuste multiselect

--- countries.py
+++ countries.py
@@ -12,10 +12,8 @@ def countries_page():
 
     with st.container():
         continent = st.radio("Selecione um continente", continents, horizontal=True)
-        #paises = sorted(df.query('Continent == @continent')['country'].unique())
-        #pais = st.multiselect("Selecione um país", paises, placeholder="Selecione")
-        #all = st.checkbox("Mostrar todos países")
-
-        show_grafico1(continent)
-
-
+        all = st.checkbox("Mostrar todos países", value=True)
+        paises = sorted(df.query('Continent == @continent')['country'].unique())
+        paises_selecionados = st.multiselect("Selecione um ou mais países", paises, placeholder="Selecione", disabled=all, default=paises[0])
+        
+        show_grafico1(paises_selecionados, continent, all)
--- utils_countries.py
+++ utils_countries.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-def show_grafico1(continent):
+def show_grafico1(paises, continent, all):
     df = pd.read_csv('df_cl_ccc_resumido.csv')
-    df_filtered = df.query('Continent == @continent').groupby('country')[['Alimentacao_Basica', 'Transp_Privado', 'Moradia_CentroCidade', 'Final_de_Semana']].mean().reset_index()
+
+    if all:
+        df_filtered = df.query('Continent == @continent').groupby('country')[['Alimentacao_Basica', 'Transp_Privado', 'Moradia_CentroCidade', 'Final_de_Semana']].mean().reset_index()
+    else:
+        df_filtered = df.query('country == @paises').groupby('country')[['Alimentacao_Basica', 'Transp_Privado', 'Moradia_CentroCidade', 'Final_de_Semana']].mean().reset_index()
+    
     df_filtered.rename(columns = {'Alimentacao_Basica':'Alimentacao basica', 'Transp_Privado':'Transp privado', 
                                  'Moradia_CentroCidade':'Moradia - centro da cidade', 'Final_de_Semana':'Final de semana'}, inplace = True)
 
