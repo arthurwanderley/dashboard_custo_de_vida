d2d3db672b1c4fa2805e44d7562382a339d27ed3
Arthur Wanderley Ferreira Dos Santos <arthur.wanderley@ons.org.br>
2024-08-04 11:23:03 -0300

    Descrição do que foi adicionado ou modificado

--- dash.py
+++ dash.py
@@ -3,6 +3,7 @@ from home import home_page
 from continents import continents_page
 from countries import countries_page
 from cities import cities_page
+from teste_arthur import main_teste_arthur
 
 st.set_page_config(
     page_title='PAGE_TITLE',
@@ -15,5 +16,6 @@ pg = st.navigation([
     st.Page(continents_page, title="Continentes", icon="🗺️"),
     st.Page(countries_page, title="Países", icon=":material/map:"),
     st.Page(cities_page, title="Cidades", icon=":material/maps_home_work:"),
+    st.Page(main_teste_arthur, title="Teste")
 ])
 pg.run()
